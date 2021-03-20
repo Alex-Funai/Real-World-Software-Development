871c8b5c016e51f5b85cc62e1483652f545d44d9
Alexander Funai <AlexanderFunai@protonmail.com>
2021-03-20 04:24:42 -0700

    1_BankStatementAnalyzer CM00

--- 1_BankStatementAnalyzer/src/BankStatementAnalyzer/BankStatementAnalyzer/Main_Application.java
+++ 1_BankStatementAnalyzer/src/BankStatementAnalyzer/BankStatementAnalyzer/Main_Application.java
@@ -1,3 +1,17 @@
+// Alexander Kitaro Funai -- March 120 2021
+// Real-World Software Development -- Rauol-Gabriel Urma & Richard Warburton
+// Chapter 2 -- The Bank Statements Analyzer
+
+
+/*   DESCRIPTION:  This program Will parse through CSV, XML,  and JSON >> leveraging commas as delimitter/separator
+  \*  date (dd-mm-yyyy) , double value (the transactiono amount), and string (transaction description).
+  */
+
+/*  REPRO STEPS:
+  *  1. Edit the file name in the bankStatementAnalyzer class  under "fileName" object.
+  *  2. Run main.
+  */
+
 package BankStatementAnalyzer;
 
 public class Main_Application {
