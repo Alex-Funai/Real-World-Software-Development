b8e8cd6a0751408ae1f7bfbd9703f275e2cc30f4
Alexander <alexanderfunai@protonmail.com>
2021-03-23 23:26:51 -0700

    BankStatementAnalyzer_CM03

--- 1_BankStatementAnalyzer/src/Main/BankStatementAnalyzer/Main_Application.java
+++ 1_BankStatementAnalyzer/src/Main/BankStatementAnalyzer/Main_Application.java
@@ -30,20 +30,22 @@ public class Main_Application {
         final Interface_BankStatementParser interfaceBankStatementParser
                 = new BankStatementCsvParser();
 
+        final Interface_Exporter interfaceExporter
+                = new HtmlExporter();
+
         /* "Interface_Exporter" method is for constructing and delcaring a new Interface_Exporter as the variable interfaceExporter.
          *  This constructor manages and holds the methods and classes we which to access, and then use on our data.
          *  In our scenario, we have designated a unique exporter for different file types, so we can simply managae the exporter class
          *  to change request. They all inherit a basis construction for the data types they should have, and we can make changes to them
          *  specifically without having to jeopardize the code base/structure of the program as a whole.
          */
-        final Interface_Exporter interfaceExporter
-                = new HtmlExporter();
 
-        /* " bankStatementAnalyzer.analyze" method
-         *
-         */
+
+        // Standard CSV parser:
         bankStatementAnalyzer.analyze("ExampleStatements2.csv", interfaceBankStatementParser);
-        System.out.println("Begin HTML export below");
+
+        // HTML Parser:
+        System.out.print("\n Begin HTML export below \n");
         bankStatementAnalyzer.analyze("bank-data-simple.html", interfaceBankStatementParser);
 
     }
