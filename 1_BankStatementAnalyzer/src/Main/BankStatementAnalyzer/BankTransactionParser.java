51654affc84133daac6872240e4ae2c31ae78fc6
Alexander <alexanderfunai@protonmail.com>
2021-03-23 23:24:04 -0700

    BankStatementAnalyzer_CM03

---
+++ 1_BankStatementAnalyzer/src/Main/BankStatementAnalyzer/BankTransactionParser.java
@@ -0,0 +1,25 @@
+// Alexander Kitaro Funai -- March 20, 2021
+// Real-World Software Development -- Rauol-Gabriel Urma & Richard Warburton
+// Chapter 2  and  Chapter 3 -- The Bank Statements Analyzer
+
+
+// Parser for file type variants.
+
+package BankStatementAnalyzer;
+
+public class BankTransactionParser {
+    public BankTransaction parseFromCSV(final String line) {
+        // ...
+        throw new UnsupportedOperationException();
+    }
+
+    public BankTransaction parseFromJSON(final String line) {
+        // ...
+        throw new UnsupportedOperationException();
+    }
+
+    public BankTransaction parseFromXML(final String line) {
+        // ...
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ 1_BankStatementAnalyzer/src/Main/BankStatementAnalyzer/Interface_BankStatementCsvParser.java
@@ -0,0 +1,13 @@
+// Alexander Kitaro Funai -- March 20, 2021
+// Real-World Software Development -- Rauol-Gabriel Urma & Richard Warburton
+// Chapter 2  and  Chapter 3 -- The Bank Statements Analyzer
+
+// Interface_BankStatementCsvParser:
+
+package BankStatementAnalyzer;
+
+import java.util.List;
+
+public interface Interface_BankStatementCsvParser {
+    List<BankTransaction> parseLinesFromCSV(List<String> lines);
+}
