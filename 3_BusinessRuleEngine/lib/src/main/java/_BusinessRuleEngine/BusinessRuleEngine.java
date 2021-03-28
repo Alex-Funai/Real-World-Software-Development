e9101dc66f8c6ce7eb3c441b2b17906fed81bccf
Alexander <alexanderfunai@protonmail.com>
2021-03-28 00:58:33 -0700

    3_BusinessRuleEngine_CM07

---
+++ 3_BusinessRuleEngine/lib/src/main/java/_BusinessRuleEngine/BusinessRuleEngine.java
@@ -0,0 +1,68 @@
+///////////////////////////////////////////////////////////////////////////////
+// Title: The Business Rule Engine
+// Files:
+// Date: March 28, 2021
+//
+// Author: Alexander Kitaro Funai
+// Email: alexanderfunai@protonmail.com
+// Book Author: Rauol-Gabriel Urma & Richard Warburton
+// Book Title: Real-World Software Development
+// Book Section: Chapter 5
+//
+//////////////////////////// 80 columns wide //////////////////////////////////
+
+
+package _BusinessRuleEngine;
+
+
+/**
+ * (Write a succinct description of this class here. You should avoid
+ * wordiness and redundancy. If necessary, additional paragraphs should
+ * be preceded by <p>, the html tag for a new paragraph.)
+ *
+ * <p>Bugs: (a list of bugs and other problems)
+ *
+ * @author (akFunai)
+ */
+public class BusinessRuleEngine {
+
+    /**
+     * (Write a succinct description of this method here. If necessary,
+     * additional paragraphs should be preceded by <p>, the html tag for
+     * a new paragraph.)
+     *
+     * @param (parameter name) (Describe the first parameter here)
+     * @param (parameter name) (Do the same for each additional parameter)
+     * @return (description of the return value)
+     */
+    public void addAction (final Action action) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * (Write a succinct description of this method here. If necessary,
+     * additional paragraphs should be preceded by <p>, the html tag for
+     * a new paragraph.)
+     *
+     * @param (parameter name) (Describe the first parameter here)
+     * @param (parameter name) (Do the same for each additional parameter)
+     * @return (description of the return value)
+     */
+    public int count() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * (Write a succinct description of this method here. If necessary,
+     * additional paragraphs should be preceded by <p>, the html tag for
+     * a new paragraph.)
+     *
+     * @param (parameter name) (Describe the first parameter here)
+     * @param (parameter name) (Do the same for each additional parameter)
+     * @return (description of the return value)
+     */
+    public void run() {
+        throw new UnsupportedOperationException();
+    }
+
+}
